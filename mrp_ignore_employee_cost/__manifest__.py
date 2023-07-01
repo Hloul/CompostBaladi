3a5bdd666a4989a3d427b88bec557217817e656d
Wafi Chaar <wafi.chaar@bas.sarl>
2023-07-01 14:51:07 +0300

    mrp_ignore_employee_cost

---
+++ mrp_ignore_employee_cost/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "mrp_ignore_employee_cost",
+
+    'summary': """
+        Ignore employee cost in manufacturing
+        """,
+
+    'description': """
+        Don't consider employee hour cost in the manufacturing orders
+    """,
+
+    'author': "BAS",
+    'website': "https://www.bas.sarl",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Manufacturing',
+    'version': '1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['mrp', 'mrp_workorder_hr_account'],
+
+    # always loaded
+    'data': [],
+}
