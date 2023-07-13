975c905d0c5440b548af780e7780cf231b9dabad
Wafi Chaar <wafi.chaar@bas.sarl>
2023-07-13 14:27:50 +0300

    Update sale_order.py

--- auto_invoice_adjustment/models/sale_order.py
+++ auto_invoice_adjustment/models/sale_order.py
@@ -1,9 +1,20 @@
 from odoo import fields, models, api
 
 
-#class SaleOrder(models.Model):
-#    _inherit = 'sale.order'
-#
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    adjust_move_id = fields.Many2one('account.move')
+    llc_currency_rate = fields.Float(string='LLS currency rate')
+    lls_tax_amount = fields.Float(string='LLS Tax Amount')
+    lbp_currency_id = fields.Many2one('res.currency', compute='get_lbp_currency')
+
+    def get_lbp_currency(self):
+        for rec in self:
+            lbp = self.env['res.currency'].search([('name', '=', 'LBP')])
+            rec.lbp_currency_id = lbp.id
+
+
 #    def _create_invoices(self, grouped=False, final=False, date=None):
 #        moves = super(SaleOrder, self)._create_invoices(grouped=grouped, final=final, date=date)
 #        for move in moves:
