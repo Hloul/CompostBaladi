44938b935e976ea80e9259efa1d348e6e5daae26
Wafi Chaar <wafi.chaar@bas.sarl>
2023-07-13 16:18:26 +0300

    auto_invoice_adjust

--- auto_invoice_adjustment/models/sale_order.py
+++ auto_invoice_adjustment/models/sale_order.py
@@ -3,7 +3,7 @@ from odoo import fields, models, api
 
 #class SaleOrder(models.Model):
 #    _inherit = 'sale.order'
-#
+
 #    def _create_invoices(self, grouped=False, final=False, date=None):
 #        moves = super(SaleOrder, self)._create_invoices(grouped=grouped, final=final, date=date)
 #        for move in moves:
